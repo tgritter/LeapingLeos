56b22bcc4684661f27acd609ff101648fa91d362
Jamie Yurick <jyurick@ualberta.ca>
2016-10-24 22:27:09 -0600

    Added script for leo movement

--- Assets/Scripts/Leo1.cs
+++ Assets/Scripts/Leo1.cs
@@ -2,24 +2,43 @@
 using System.Collections;
 
 public class Leo1 : MonoBehaviour {
-	Vector3 pos;
+
+	Vector3 pos = new Vector3 (1, 0, 0);
+	float speed = 12F;
+	float spinTime = 20F;
+
+
 	// Use this for initialization
 	void Start () {
-		pos = this.transform.position; 
-		pos.x += 5;
-		this.transform.position += pos;
-	
+		this.transform.position = pos;
 	}
-	
+
+	void circle() {
+
+		pos.x = 10 * Mathf.Cos (2 * Mathf.PI * spinTime / 1000);
+		pos.z = 10 * Mathf.Sin (2 * Mathf.PI * spinTime / 1000);
+
+		this.transform.position = pos;
+		print (pos.x);
+		print (pos.y);
+
+
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown("left")){
-			
-		pos = this.transform.position; 
-		pos.x += 1;
-		this.transform.position += pos;
+		if(Input.GetKey("right")){
+			circle ();
+			spinTime += speed;
 		}
+
+		if (Input.GetKey ("left")) {
+			circle ();
+			spinTime -= speed;
+		}
+
 
 
 	}

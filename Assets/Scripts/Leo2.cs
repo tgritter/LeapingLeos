6687eb70470761053f88ac707441301a215c08e0
Jamie Yurick <jyurick@ualberta.ca>
2016-10-24 23:21:49 -0600

    Added more leo movement functionality

--- Assets/Scripts/Leo2.cs
+++ Assets/Scripts/Leo2.cs
@@ -1,39 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Leo1 : MonoBehaviour {
+public class Leo2 : MonoBehaviour {
 
 	Vector3 pos = new Vector3 (0, 0, 0);
-	Vector3 pos2 = new Vector3 (20, 0, 0);
 	float speed = 12F;
 	float spinTime = 20F;
-	float originX = 10;
+	float originX = 10F;
 	float originZ = 0F;
-	float radius = 10F;
-
-
-
-	public GameObject Leo2;
-
 
 
 	// Use this for initialization
 	void Start () {
 		this.transform.position = pos;
-		Leo2.transform.position = pos2;
 	}
 
 	void circle() {
 
-		pos.x = radius * Mathf.Cos (2 * Mathf.PI * spinTime / 1000) + originX;
-		pos.z = radius * Mathf.Sin (2 * Mathf.PI * spinTime / 1000) + originZ;
-
-		pos2.x = -radius * Mathf.Cos (2 * Mathf.PI * spinTime / 1000) + originX;
-		pos2.z = -radius * Mathf.Sin (2 * Mathf.PI * spinTime / 1000) + originZ;
-
+		pos.x = -10 * Mathf.Cos (2 * Mathf.PI * spinTime / 1000) + originX;
+		pos.z = -10 * Mathf.Sin (2 * Mathf.PI * spinTime / 1000) + originZ;
 
 		this.transform.position = pos;
-		Leo2.transform.position = pos2;
+		print (pos.x);
+		print (pos.y);
+
 
 	}
 
@@ -52,39 +42,26 @@ public class Leo1 : MonoBehaviour {
 		}
 		if (Input.GetKey ("d")) {
 			pos.x += 0.1F * speed;
-			pos2.x += 0.1F * speed;
 			originX += 0.1F*speed;
 			this.transform.position = pos;
-			Leo2.transform.position = pos2;
 
 		}
 		if (Input.GetKey ("a")) {
 			pos.x -= 0.1F * speed;
-			pos2.x -= 0.1F * speed;
 			originX -= 0.1F* speed;
 			this.transform.position = pos;
-			Leo2.transform.position = pos2;
 		}
 		if (Input.GetKey ("w")) {
 			pos.z += 0.1F * speed;
-			pos2.z += 0.1F * speed;
 			originZ += 0.1F* speed;
 			this.transform.position = pos;
-			Leo2.transform.position = pos2;
 		}
 		if (Input.GetKey ("s")) {
 			pos.z -= 0.1F * speed;
-			pos2.z -= 0.1F * speed;
 			originZ -= 0.1F* speed;
 			this.transform.position = pos;
-			Leo2.transform.position = pos2;
 		}
-		if (Input.GetKey ("up")) {
-			radius += 0.1F * speed;
-		}
-		if (Input.GetKey ("down")) {
-			radius -= 0.1F * speed;
-		}
+
 
 
 

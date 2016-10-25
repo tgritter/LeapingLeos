18233e5c749c758f4ed52c08e99eb21302179ac6
tgritter <travis.gritter@gmail.com>
2016-10-24 21:04:47 -0700

    Added random log spawner

---
+++ Assets/Scripts/LogSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogSpawner : MonoBehaviour {
+
+	public GameObject log;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnLog", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void SpawnLog()
+	{
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (log, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+
+	}
+}
